2757994673348a87d717b8f2110b82a3f6af3af2
mohamadsadem0 <mohamad1.sadem@gmail.com>
2025-02-12 20:26:00 +0200

    .

--- frontend/src/Pages/MessagePage.jsx
+++ frontend/src/Pages/MessagePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import gif from "../Assets/processing.gif";
+// import gif from "../Assets/processing.gif";
 import Header from "../Components/Layout/Header";
 import Footer from "../Components/Layout/Footer";
 
@@ -9,7 +9,7 @@ const MessagePage = () => {
     <>
       <Header />
       <div className="w-full flex items-center flex-col justify-center h-screen">
-        <img src={gif} alt="image" className="w-[150px] h-[150px]" />
+        <img  alt="image" className="w-[150px] h-[150px]" />
         <h1 className="text-4xl font-[700]">Comming Soon</h1>
       </div>
       <Footer />
