84759f1f33c2eb9881e41aaf207a9566988580ef
mohamadsadem0 <mohamad1.sadem@gmail.com>
2025-02-13 17:01:24 +0200

    .

--- frontend/src/Components/Layout/ResponsiveHeader/ResponsiveHeader.jsx
+++ frontend/src/Components/Layout/ResponsiveHeader/ResponsiveHeader.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { BiMenuAltLeft } from "react-icons/bi";
-import Logo from "../../../assets/logo.png";
+import Logo from "../../../Assets/logo.png";
 import { Link } from "react-router-dom";
 import {
   AiOutlineHeart,
