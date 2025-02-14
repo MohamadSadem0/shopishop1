ed7299483a39ee0825e0a54bf546f3d07f5ed62a
mohamadsadem0 <mohamad1.sadem@gmail.com>
2025-02-14 03:24:16 +0200

    .

--- backend/src/main/java/com/example/ShopiShop/security/JwtService.java
+++ backend/src/main/java/com/example/ShopiShop/security/JwtService.java
@@ -35,7 +35,7 @@ public class JwtService {
         return generateToken(claims, user);
     }
 
-    public String generateToken(Map<String, Object> extraClaims, User user) {2ertrtyqwyi    yi
+    public String generateToken(Map<String, Object> extraClaims, User user) {    
         return Jwts.builder()
                 .setClaims(extraClaims)
                 .setSubject(user.getEmail())
