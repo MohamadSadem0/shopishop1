99357c7a250aaed4117e36f3bb86342b039fecf9
mohamadsadem0 <mohamad1.sadem@gmail.com>
2025-02-14 15:37:16 +0200

    .

--- backend/src/main/java/com/example/ShopiShop/config/CustomCorsFilter.java
+++ backend/src/main/java/com/example/ShopiShop/config/CustomCorsFilter.java
@@ -24,7 +24,7 @@ public class CustomCorsFilter implements Filter {
 //        response.setHeader("Access-Control-Allow-Origin", "http://localhost:5500");
 
 //        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-        response.setHeader("Access-Control-Allow-Origin", "http://84.8.108.111:3000");
+        response.setHeader("Access-Control-Allow-Origin", "https://stunning-gelato-710ab3.netlify.app/");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
         response.setHeader("Access-Control-Allow-Credentials", "true");  // Must be exactly 'true'
