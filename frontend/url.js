05902c039147ca3b84a609c42afbd24f468e4deb
mohamadsadem0 <mohamad1.sadem@gmail.com>
2025-02-18 20:56:30 +0200

    .

--- frontend/url.js
+++ frontend/url.js
@@ -1,3 +1,3 @@
-// const baseURL = "http://84.8.108.111:8080"; // Default to local API if env variable is missing
-const baseURL = "http://localhost:8080"; // Default to local API if env variable is missing
+const baseURL = "http://84.8.108.111:8080"; // Default to local API if env variable is missing
+// const baseURL = "http://localhost:8080"; // Default to local API if env variable is missing
 export default baseURL
