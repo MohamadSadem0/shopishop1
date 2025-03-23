749e9d877af1fa0a9b5db5186f66bf00d92cbbc1
mohamadsadem0 <mohamad1.sadem@gmail.com>
2025-03-24 00:40:07 +0200

    .

--- backend/src/main/java/com/example/ShopiShop/config/CorsConfig.java
+++ backend/src/main/java/com/example/ShopiShop/config/CorsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer{
     @Value("${app.frontend-url}")
     private String frontendUrl;
 
