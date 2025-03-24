5e123c481cd348de1ad0130a21496558250a1587
mohamadsadem0 <mohamad1.sadem@gmail.com>
2025-03-24 23:28:16 +0200

    .

--- frontend/url.js
+++ frontend/url.js
@@ -1,4 +1,4 @@
 // const baseURL = "https://84.8.108.111:8443"; // Default to local API if env variable is missing
-const baseURL = "http://shopishop.duckdns.org"; // Default to local API if env variable is missing
+const baseURL = "https://shopishop.duckdns.org"; // Default to local API if env variable is missing
 // const baseURL = "http://localhost:8080"; // Default to local API if env variable is missing
 export default baseURL
